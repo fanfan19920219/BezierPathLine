308f8c1b6239c880da18b01ae4bb2a6df0cd18a6
zhangzhihua19920219 <406608969@qq.com>
2016-07-05 10:14:31 +0800

    Initial Commit

---
+++ Sci_Line/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sci_Line
+//
+//  Created by M-SJ077 on 16/7/5.
+//  Copyright © 2016年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sci_Line/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sci_Line
+//
+//  Created by M-SJ077 on 16/7/5.
+//  Copyright © 2016年 zhangzhihua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
